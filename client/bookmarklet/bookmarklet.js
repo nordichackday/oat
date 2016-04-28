23adaa7e13eeb1cd47314a71852a5e87ac7eb080
Fredrik Höglund <fredrik.hoglund@svt.se>
2016-04-28 15:39:38 +0200

    Some basic bookmarklet-structure.

---
+++ client/bookmarklet/bookmarklet.js
@@ -0,0 +1,7 @@
+(function() {
+  window.__OATCONFIG = {};
+  var oatScript=document.createElement('SCRIPT');
+  oatScript.type='text/javascript';
+  oatScript.src='localhost:9000/bookmarklet/dist/oat-loader.js';
+  document.getElementsByTagName('head')[0].appendChild(oatScript);
+})();
--- client/bookmarklet/index.js
+++ client/bookmarklet/index.js
@@ -1,2 +1,17 @@
 import getAllElementsWithAttribute from './utils/getAllElementsWithAttribute';
 import getPositionAndSize from './utils/getPositionAndSize';
+
+var oatConfig = window.__OATCONFIG || {
+  plugins: []
+};
+
+function loadDependencies(plugins) {
+
+}
+
+function initializeBookmarklet(oatConfig) {
+
+}
+
+loadDependencies(oatConfig.plugins);
+initializeBookmarklet(oatConfig);
