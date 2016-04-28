b68a046ca4a719e708a7726509c4c42e694c6418
Fredrik Höglund <fredrik.hoglund@svt.se>
2016-04-28 16:52:14 +0200

    Converted Gruntfile to tabs.

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -1,39 +1,39 @@
 module.exports = function(grunt) {
-  require('load-grunt-tasks')(grunt);
+	require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
-    browserify: {
-      dist: {
-        options: {
-          transform: [
-            ["babelify", {
-              sourceMap: true,
-              presets: ['babel-preset-es2015']
-            }]
-          ]
-        },
-        files: {
-          // if the source file has an extension of es6 then
-          // we change the name of the source file accordingly.
-          // The result file's extension is always .js
-          "./bookmarklet/dist/oat-loader.js": ["./bookmarklet/index.js"]
-        }
-      }
-    },
-    watch: {
-      scripts: {
-        files: ["bookmarklet/**/*.js", '!**/dist/**/*.js'],
-        tasks: ["browserify"]
-      }
-    },
-    connect: {
-      server: {
-        options: {
-          port: 9000
-        }
-      }
-    }
-  });
+	grunt.initConfig({
+		browserify: {
+			dist: {
+				options: {
+					transform: [
+						["babelify", {
+							sourceMap: true,
+							presets: ['babel-preset-es2015']
+						}]
+					]
+				},
+				files: {
+					// if the source file has an extension of es6 then
+					// we change the name of the source file accordingly.
+					// The result file's extension is always .js
+					"./bookmarklet/dist/oat-loader.js": ["./bookmarklet/index.js"]
+				}
+			}
+		},
+		watch: {
+			scripts: {
+				files: ["bookmarklet/**/*.js", '!**/dist/**/*.js'],
+				tasks: ["browserify"]
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					port: 9000
+				}
+			}
+		}
+	});
 
-  grunt.registerTask('default', ['browserify', 'connect', 'watch']);
+	grunt.registerTask('default', ['browserify', 'connect', 'watch']);
 };
