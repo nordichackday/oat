1960ee33c790be9bb0ccdde9661085eb0b49a9d3
Fredrik Höglund <fredrik.hoglund@svt.se>
2016-04-28 14:42:40 +0200

    Some first helpers.

---
+++ client/bookmarklet/helpers/getPositionAndSize.js
@@ -0,0 +1,15 @@
+var getPositionAndSize = function(element) {
+	var top = 0, left = 0;
+	do {
+		top += element.offsetTop || 0;
+		left += element.offsetLeft || 0;
+		element = element.offsetParent;
+	} while(element);
+
+	return {
+		top: top,
+		left: left,
+		width: element.offsetWidth,
+		height: element.offsetHeight
+	};
+};
